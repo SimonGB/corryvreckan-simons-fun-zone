5b8a928a865f13c5426628129bb187d951c34630
Finn Feindt <finn.feindt@desy.de>
2023-01-03 11:33:43 +0100

    AlignmentTime: Creating detector type module skeleton.

---
+++ src/modules/AlignmentTime/AlignmentTime.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * @brief Implementation of module AlignmentTime
+ *
+ * @copyright Copyright (c) 2020 CERN and the Corryvreckan authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+#include "AlignmentTime.h"
+
+using namespace corryvreckan;
+
+AlignmentTime::AlignmentTime(Configuration& config, std::shared_ptr<Detector> detector)
+    : Module(config, std::move(detector)) {}
+
+void AlignmentTime::initialize() {
+
+    for(auto& detector : get_detectors()) {
+        LOG(DEBUG) << "Initialise for detector " + detector->getName();
+    }
+
+    // Initialise member variables
+    m_eventNumber = 0;
+}
+
+StatusCode AlignmentTime::run(const std::shared_ptr<Clipboard>&) {
+
+    // Loop over all detectors
+    for(auto& detector : get_detectors()) {
+        // Get the detector name
+        std::string detectorName = detector->getName();
+        LOG(DEBUG) << "Detector with name " << detectorName;
+    }
+
+    // Increment event counter
+    m_eventNumber++;
+
+    // Return value telling analysis to keep running
+    return StatusCode::Success;
+}
+
+void AlignmentTime::finalize(const std::shared_ptr<ReadonlyClipboard>&) {
+
+    LOG(DEBUG) << "Analysed " << m_eventNumber << " events";
+}
---
+++ src/modules/AlignmentTime/AlignmentTime.h
@@ -0,0 +1,55 @@
+/**
+ * @file
+ * @brief Definition of module AlignmentTime
+ *
+ * @copyright Copyright (c) 2020 CERN and the Corryvreckan authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+#include <TCanvas.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <iostream>
+#include "core/module/Module.hpp"
+#include "objects/Cluster.hpp"
+#include "objects/Pixel.hpp"
+#include "objects/Track.hpp"
+
+namespace corryvreckan {
+    /** @ingroup Modules
+     * @brief Module to do function
+     *
+     * More detailed explanation of module
+     */
+    class AlignmentTime : public Module {
+
+    public:
+        /**
+         * @brief Constructor for this unique module
+         * @param config Configuration object for this module as retrieved from the steering file
+         * @param detector Pointer to the detector for this module instance
+         */
+        AlignmentTime(Configuration& config, std::shared_ptr<Detector> detector);
+
+        /**
+         * @brief [Initialise this module]
+         */
+        void initialize() override;
+
+        /**
+         * @brief [Run the function of this module]
+         */
+        StatusCode run(const std::shared_ptr<Clipboard>& clipboard) override;
+
+        /**
+         * @brief [Finalise module]
+         */
+        void finalize(const std::shared_ptr<ReadonlyClipboard>& clipboard) override;
+
+    private:
+        int m_eventNumber;
+    };
+
+} // namespace corryvreckan
