f0999831ee08c42b116f2f4553c251ed33f3ab6b
Simon Spannagel <simon.spannagel@cern.ch>
2023-01-24 09:50:28 +0100

    Fix licenses for Peary files and revert accidential changes

--- src/modules/EventLoaderCLICpix2/CLICpix2/clicpix2_utilities.cpp
+++ src/modules/EventLoaderCLICpix2/CLICpix2/clicpix2_utilities.cpp
@@ -1,13 +1,3 @@
-/**
- * @file
- * @brief CLICpix2 utilities implementation
- *
- * @copyright Copyright (c) 2022 CERN and the Corryvreckan authors.
- * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
- * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
- * Intergovernmental Organization or submit itself to any jurisdiction.
- */
-
 #include "clicpix2_utilities.hpp"
 #include <fstream>
 #include "core/utils/log.h"
