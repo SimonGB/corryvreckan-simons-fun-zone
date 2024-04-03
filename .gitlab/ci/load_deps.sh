c91caf9db063cab58bdb46ced61ebf07d87c811e
Simon Gross-Boelting <s.gross-boelting@cern.ch>
2024-04-03 10:39:29 +0200

    Added histograms and changed binning

--- .gitlab/ci/load_deps.sh
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2017-2022 CERN and the Allpix Squared authors
-# SPDX-License-Identifier: MIT
-
-source $ROOTSYS/bin/thisroot.sh
--- testing/run_test.sh
+++
@@ -1,7 +0,0 @@
-ABSOLUTE_PATH="$( cd "$( dirname "${BASH_SOURCE}" )" && pwd )"
-
-# First argument is the data set to be used, ask for the download:
-python download_data.py $1
-
-# Second argument is the full test command to be executed:
-exec $2
